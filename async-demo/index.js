10811ae2b12136380082f2966308cf61367ce248
Swarup Hegde <swaruphegde@gmail.com>
2020-05-23 18:22:30 +0530

    feat: callbacks

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,41 +1,32 @@
-async function display() {
-  try {
-        const customer = await getCustomer(1);
-        if (customer.isGold) {
-            console.log('Customer:', customer);
-            const movies = await getMovies(['movie1, movie2']);
-            console.log(`Movies: ${movies}`);
-            const email = await sendEmail(customer.email, movies);
-            console.log(email);
-        }
-  }
-  catch(err){
-      console.log('Error: ', err)
-  }
-}
+console.log('Before')
+getUser(1, user => {
+    getRepositories(user.gitHubUsername, (repos) => {
+        getCommits(repos, (commits) => {
+            console.log('Commits: ', commits)
+        })
+    })
+})
+console.log('After')
 
-display()
 
-function getCustomer(id) {
-  return new Promise((resolve, reject) => {
+function getUser(id, callback){
+    setTimeout(()=>{
+        console.log('Reading a user from database...')
+        callback({ id: id, gitHubUsername: 'indraasura' }) 
+    }, 2000)
+}   
+
+function getRepositories(username, callback){
     setTimeout(() => {
-      resolve({ id: id, name: "Mosh Hamedani", isGold: true, email: "email" });
-    }, 2000);
-  });
+        console.log('Reading repos from ' + username)
+        callback(['repo1', 'repo2', 'repo3'])
+    }, 2000)
 }
 
-function getMovies(movies) {
-  return new Promise((resolve, reject) => {
+function getCommits(repos, callback){
     setTimeout(() => {
-      resolve(movies);
-    }, 2000);
-  });
+        console.log('Reading commits from ' + repos[0])
+        callback(['commit1'])
+    }, 2000)
 }
 
-function sendEmail(email, movies) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("Email sent...");
-    }, 2000);
-  });
-}
