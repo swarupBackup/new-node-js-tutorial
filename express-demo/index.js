51494e8c13b26f4fb5c0870fb32cd12771708f95
Swarup Hegde <swaruphegde@gmail.com>
2020-10-07 12:11:41 +0530

    feat: put and delete routes added:

--- express-demo/index.js
+++ express-demo/index.js
@@ -36,6 +36,26 @@ app.post('/api/courses', (req, res) => {
     res.send(courses)
 })
 
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find( c => c.id === parseInt(req.params.id) )
+    if(!courses) return res.status(404).send('Requested course was not found')
+    const schema = {
+        name: Joi.string().min(3).require()
+    }
+    const result = Joi.validate(schema, req.body)
+    if(result.error) return res.status(400).send(result.error.details[0].message)
+    course.name = req.body.name
+    res.send(courses)
+})
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find( c => c.id === parseInt(req.params.id) )
+    if(!courses) return res.status(404).send('Requested course was not found')
+    const index = courses.indexOf(course)
+    courses.splice(index, 1)
+    res.send(courses)
+})
+
 const port = process.env.PORT || 3000
 
 app.listen(3000, () => { console.log(`Listening on port ${port}...`) })
