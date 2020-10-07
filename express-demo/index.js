d18f5d215ee5003680be8e9f13ff7dce6a04f3d8
Swarup Hegde <swaruphegde@gmail.com>
2020-10-07 11:41:40 +0530

    feat: get routes

---
+++ express-demo/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+const courses = [
+    {id: 1, name: "course1"},
+    {id: 2, name: "course2"},
+    {id: 3, name: "course3"}
+]
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.listen(3000, () => { console.log('Listening on port 3000...') })
